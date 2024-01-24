441b7210d8f51d858f6f43d6ebcb74b26983e4c3
Hutass <huntingrab@gmail.com>
2024-01-24 14:46:29 +0300

    Add project files.

---
+++ Peceptron/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Peceptron
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DatasetGenerator datasetGenerator = new(@"D:\DATASET_AILAB");
+            datasetGenerator.CreateSymbol("0", 200, 30,7);
+            datasetGenerator.CreateSymbol("1", 200, 30,7);
+        }
+    }
+}
